4d658a314ad216b1d291ad24075e82994bba01d6
juandelgado4-droid <juan.delgado4@utp.edu.co>
2026-05-02 17:59:34 -0500

    Remove swipe gesture detection for mobile
    Removed swipe gesture detection and associated event listeners.

--- js/javascript.js
+++ js/javascript.js
@@ -6,39 +6,3 @@ function showPanel(id, btn) {
   btn.classList.add('active');
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-// Swipe gesture detection for mobile
-let touchStartX = 0;
-let touchEndX = 0;
-
-function handleSwipe() {
-  const threshold = 50;
-  const diff = touchStartX - touchEndX;
-  const activeTab = document.querySelector('.tab.active');
-
-  if (Math.abs(diff) > threshold) {
-    if (diff > 0) {
-      // Swipe left - move to next tab
-      const nextTab = activeTab.nextElementSibling;
-      if (nextTab && nextTab.classList.contains('tab')) {
-        nextTab.click();
-      }
-    } else {
-      // Swipe right - move to previous tab
-      const prevTab = activeTab.previousElementSibling;
-      if (prevTab && prevTab.classList.contains('tab')) {
-        prevTab.click();
-      }
-    }
-  }
-}
-
-// Add touch listeners to page for swipe support
-document.addEventListener('touchstart', e => {
-  touchStartX = e.changedTouches[0].screenX;
-}, false);
-
-document.addEventListener('touchend', e => {
-  touchEndX = e.changedTouches[0].screenX;
-  handleSwipe();
-}, false);
